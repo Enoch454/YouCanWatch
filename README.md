0e8c3c24ebd3fda809d4efb015f3669ef1124a79
enoch454 <enoch454>
2021-10-21 09:19:32 -0500

    version 0

---
+++ README.md
@@ -0,0 +1,6 @@
+# YouCanWatch ...
+La app que si tenia que hacer de tarea
+
+## Notas
+* En ocaciones no muestra el contenido que deberia por una falla con el origen de los datos.
+* El diseño responsivo es mi pasión xD
---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+//const jikanjs  = require('jikanjs')
+
+const vue_app = Vue.createApp({
+    data(){
+        return {
+            animeImageURL: "../../assets/img/E-C9YyhVcAE1Coa.jpg",
+            animeTitle: "Another title anime",
+            animeDescription: "Lorem ipsum tolendo tolens uwu",
+            linkMoreInfo: "https://www.google.com",
+            animeObject: null,
+        }
+    },
+    methods:{
+        async getRandomAnime(){
+            console.log('---');
+            console.log('se busca anime al azar');
+            console.log('---');
+            var random_id = Math.round(Math.random()*(49999-1)+1);
+
+
+            //  jikanjs.loadAnime(19815, 'episodes').then((response) => {this.animeObject = response} );
+            //var random_id = 49999;
+
+            do {
+
+                var api_irl = "https://api.jikan.moe/v3/anime/"+random_id;
+                const res = await fetch(api_irl)
+                    .then(response => {
+                        console.log('-----response-----');
+                        console.log(response)
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('-----data-----');
+                        console.log(data);
+                        this.animeObject = data;
+                    });
+    
+
+            } while (this.animeObject.status == 404);
+            
+                
+            
+            this.linkMoreInfo = this.animeObject.url;
+            this.animeTitle = this.animeObject.title;
+            this.animeDescription = this.animeObject.synopsis;
+            this.animeImageURL = this.animeObject.image_url;
+        },
+    },
+    mounted(){
+        this.getRandomAnime();
+    },
+}).mount("#recomendation_card");
